780f4d2cdfc78871cd0282a747e04f4d4fe28a58
fertlopus <marat.movlamov@icloud.com>
2023-11-09 19:26:37 +0100

    Updated LR algo via reusable class of SGD

--- src/main/scala/machinelearning/LinearRegression.scala
+++ src/main/scala/machinelearning/LinearRegression.scala
@@ -1,29 +1,30 @@
 package machinelearning
 import machinelearning.MyCSVReader
+import machinelearning.SGD
 import java.io.File
 
 class LinearRegression {
   var weights: Array[Double] = _
   var bias: Double = scala.util.Random.nextDouble()
+  val sgdOptimizer = new SGD(learningRate = 0.01)
 
-  def fit(X: Array[Array[Double]], y: Array[Double], learningRate: Double, epochs: Int): Unit = {
+  def fit(X: Array[Array[Double]], y: Array[Double], epochs: Int): Unit = {
       val nFeatures = X(0).length
-      weights = Array.fill(nFeatures)(bias)
+      weights = Array.fill(nFeatures)(0.0)
 
-      for (i <- 1 to epochs) {
+    for (_ <- 1 to epochs) {
         for (j <- X.indices) {
             val prediction = predict(X(j))
             val error = prediction - y(j)
-            bias -= learningRate * error
-            for (k <- weights.indices) {
-                weights(k) -= learningRate * error * X(j)(k)
-            }
-        }
+            val gradient = X(j).map(xi => 2 * xi * error)
+            val biasGradient = 2 * error
+        weights = sgdOptimizer.step(weights, gradient)
+        bias -= sgdOptimizer.learningRate * biasGradient
       }
+    }
   }
 
   def predict(x: Array[Double]): Double = {
-      weights.zip(x).map {
-        case (w, xi) => w * xi}.sum + bias
+    weights.zip(x).map { case (w, xi) => w * xi }.sum + bias
   }
 }
